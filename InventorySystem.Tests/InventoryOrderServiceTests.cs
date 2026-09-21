42646c566cbc609afb4845b7827b0f8f693f8830
Lateefat Obisesan <temilele06@gmail.com>
2026-09-21 06:27:19 -0500

    building the first test case.

--- InventorySystem.Tests/InventoryOrderServiceTests.cs
+++ InventorySystem.Tests/InventoryOrderServiceTests.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
+using InventorySystem;
 
 namespace InventorySystem.Tests
 {
-    internal class InventoryOrderServiceTests
+    public class InventoryOrderServiceTests
     {
+       
     }
 }
